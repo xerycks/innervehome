9e8bf022ccb49a799a4c78c8ac8e67105892e33f
Rishabh Rathore <56212029+xerycks@users.noreply.github.com>
2020-06-18 14:36:51 +0530

    lets see

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  <a name="logo" href="assets/img/card.png"><img src="assets/img/card.png" alt="Innerve 5.o" width="200"></a>
+  <br>
+  Innerve 5.0 Website Documentation
+</h1>
+<h4 align="center">Be sure to :star: , With :heart: from OSS Club, AIT</h4>
+<h4 align="center">:hammer: in Progress</h4>
---
+++ assets/js/main.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
+
+  //methods
+  $.fn.fullpage.setAllowScrolling(true);
+});
+
+$(function () {
+  $(".material-card > .mc-btn-action").click(function () {
+    var card = $(this).parent(".material-card");
+    var icon = $(this).children("i");
+    icon.addClass("fa-spin-fast");
+
+    if (card.hasClass("mc-active")) {
+      card.removeClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-arrow-left")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-bars");
+      }, 800);
+    } else {
+      card.addClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-bars")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-arrow-left");
+      }, 800);
+    }
+  });
+});
+$(function () {
+  $(".material-card > .mc-btn-action").click(function () {
+    var card = $(this).parent(".material-card");
+    var icon = $(this).children("i");
+    icon.addClass("fa-spin-fast");
+
+    if (card.hasClass("mc-active")) {
+      card.removeClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-arrow-left")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-bars");
+      }, 800);
+    } else {
+      card.addClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-bars")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-arrow-left");
+      }, 800);
+    }
+  });
+});
+
+//gallery
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 5,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+});
+
+$(function () {
+  $(".material-card > .mc-btn-action").click(function () {
+    var card = $(this).parent(".material-card");
+    var icon = $(this).children("i");
+    icon.addClass("fa-spin-fast");
+
+    if (card.hasClass("mc-active")) {
+      card.removeClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-arrow-left")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-bars");
+      }, 800);
+    } else {
+      card.addClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-bars")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-arrow-left");
+      }, 800);
+    }
+  });
+});
+
+
+// Cursor
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500)
+})
